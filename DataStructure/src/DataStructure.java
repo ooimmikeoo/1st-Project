152fe3861ed5ffc8eca05c9f70808c0b15fb96f3
Michael Sturdivant <michael.sturdivant@rocky.edu>
2016-09-12 08:48:20 -0600

    getting ready to do work

--- DataStructure/src/DataStructure.java
+++ DataStructure/src/DataStructure.java
@@ -1,6 +1,41 @@
 
 public class DataStructure<T> {
-	public static void main(String[] args){
-		
+	private String name;
+	private int age;
+	// constructor array
+	DataStructure(){
+		int[] myArray= int[];
+
 	}
+	//create the names and ages, and store them in the array
+	DataStructure(String s, int n){
+myArray[]= (string s, int n);
+
+
+	}
+	//change the string, essentially a sort for the array
+	public String toString(){
+
+		return name;	
+	}
+	//name the slots in the array
+	public void setName(){
+
+	}
+	//give the indexes in array an age
+	public void setAge(){
+
+	}
+	//return the name
+	public String getName(){
+
+		return name;
+	}
+	//return the age
+	public int getAge(){
+
+		return age;
+	}
+
 }
+
